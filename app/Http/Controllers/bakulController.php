3dfd4a3eaee985558e230c0b76183e23c4729312
BakulIslam <89521229+BakulIslam@users.noreply.github.com>
2025-03-01 17:23:05 +0600

    Add files via upload

---
+++ app/Http/Controllers/bakulController.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\Http\Controllers;
+
+use app\DataTables\CategoryDataTables;
+use App\Helpers\Traits\RowIndex;
+use App\Http\Controllers\Controller;
+
+class bakulController extends Controller{
+    use RowIndex;
+    public function idnex(){
+
+    }
+    
+}
